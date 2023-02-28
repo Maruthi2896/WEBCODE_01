f96a950516e623395b2b19ccb376e7af0b340faf
Maruthi K J <123438736+Maruthi2896@users.noreply.github.com>
2023-02-28 16:25:10 +0530

    Add files via upload
    finalised

---
+++ script.js
@@ -0,0 +1,118 @@
+
+let nav=document.createElement("nav");
+nav.innerText=" GUVI ZEN FSD WEBCODE-1"
+nav.setAttribute("class",'navbar');
+nav.style.backgroundColor="#c5aa6a"
+nav.style.color='white'
+nav.style.fontSize='20px'
+nav.style.height="40px"
+nav.style.marginTop='0px'
+document.body.append(nav);
+
+let h1=document.createElement("h1");
+h1.innerText="Enter the name to search country";
+h1.style.color='blue';
+h1.style.marginLeft='100px'
+h1.style.fontFamily='Ariel'
+document.body.append(h1);
+
+let input=document.createElement("input");
+input.innerText="Enter the name to search country";
+input.setAttribute("type",'text')
+input.setAttribute("id",'name')
+input.style.marginLeft='100px'
+input.style.padding='3px'
+input.style.width='440px'
+input.style.height='40px'
+
+document.body.append(input);
+
+let input1=document.createElement("input");
+input1.setAttribute("type",'submit')
+input1.setAttribute("onclick","getNationalise()");
+input1.style.padding='3px'
+input1.style.width='100px'
+input1.style.color='yellow';
+input1.style.height='40px'
+input1.style.marginLeft='20px'
+input1.style.backgroundColor="grey"
+
+document.body.append(input1);
+
+let div=document.createElement("div");
+div.setAttribute("id","details")
+document.body.append(div);
+
+var footer=document.createElement("footer");
+footer.innerText="By maruthi K j"
+footer.style.color='grey'
+footer.style.marginLeft='300px'
+footer.style.marginTop='60px'
+document.body.append(footer);
+
+
+async function getNationalise(){
+    try{
+
+        var name=document.getElementById('name').value
+        console.log(name)
+        let res= await fetch(`https://api.nationalize.io/?name=${name}`);
+        let data=await res.json();
+        //console.log(data);
+
+        let details=document.getElementById('details');
+       details.innerHTML=`
+       <div >
+       <label for="searchFilter" style="margin-left:180px">search</label>
+       <input id="searchInput"style='margin-left:30px;margin-top:30px;' type="text">
+       <div class="row data ">
+
+       <div class="col-md-4 mt-30 image">
+        <img src="https://t4.ftcdn.net/jpg/02/60/74/69/360_F_260746971_m4dHGLCIl22kp3wu01tU32677ca65OZi.jpg" class="img-fluid rounded-start ml-3" alt="...">
+        </div>
+        <div class="col-md-8">
+
+        <div class="filterItems">
+
+          <p id="item1"class="card-title fItems" style="font-size:40px">Name: ${name}</p>
+          <p  id="item2"class="card-text m-0 fItems "><small class="text-muted">Country:Id-1 <b>${data.country[0].country_id}</b> And Probability is <b>${data.country[0].probability*100}%</b></small></p>
+          <p id="item3" class="card-text m-0 fItems"><small class="text-muted">Country:Id-2 <b>${data.country[1].country_id} </b>And  Probability is <b>${data.country[1].probability*100}%</b></small></p>
+          <p  id="item4"class="card-text m-0 fItems "><small class="text-muted">Country:Id-1 <b>${data.country[2].country_id}</b> And Probability is <b>${data.country[2].probability*100}%</b></small></p>
+         
+          </div>
+      </div>
+      </div>
+    </div>
+     `
+    var search=document.querySelector("searchInput");
+    search.addEventListener("onkeyup",(e)=>{
+        var input, filter, items, fItems, a, i, txtValue;
+        console.log(e.target.value)
+        input = document.getElementById("searchInput");
+        filter = input.value.toUpperCase();
+        items = document.getElementById("filterItems");
+        fItems = document.getElementsByClassName("fItems");
+        for (i = 0; i < fItems.length; i++) {
+            a = fItems[i].getElementsByTagName("a")[0];
+            txtValue =  a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                fItems[i].style.display = "";
+            } else {
+                fItems[i].style.display = "none";
+            }
+        }
+    });
+   
+
+    }
+    catch(err){
+        console.log(err);
+    }
+    
+}
+
+
+
+ 
+
+   
